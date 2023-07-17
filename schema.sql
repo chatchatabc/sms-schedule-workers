96754029e4de313568240e2ce939e0509b7fa551
BoJoNVi <bonjomontes@gmail.com>
2023-07-17 10:22:11 +0800

    build: added scripts to automate initialization of DB

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (userId INTEGER PRIMARY KEY, username TEXT, password TEXT);
-INSERT INTO users (userId, username, password) VALUES (1, 'admin', 'e4d2f949a401c04e9cd0bd410e31d6f81b413974151fc458c18d2d186c379219');
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, createdAt TEXT, updatedAt TEXT);
+INSERT INTO users (id, username, password, createdAt, updatedAt) VALUES (2, 'admin', 'e4d2f949a401c04e9cd0bd410e31d6f81b413974151fc458c18d2d186c379219','2023-07-17T02:19:59.107Z','2023-07-17T02:19:59.107Z');
+
+DROP TABLE IF EXISTS schedules;
+CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, date TEXT, text TEXT, createdAt TEXT, updatedAt TEXT);
