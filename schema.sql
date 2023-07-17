10f0d307ccb6c2fd6c77e40bfa2ec13c531d8d3d
BoJoNVi <bonjomontes@gmail.com>
2023-07-17 13:05:06 +0800

    build: added initial entry for the schedules schema

--- schema.sql
+++ schema.sql
@@ -3,4 +3,5 @@ CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, passwor
 INSERT INTO users (id, username, password, createdAt, updatedAt) VALUES (2, 'admin', 'e4d2f949a401c04e9cd0bd410e31d6f81b413974151fc458c18d2d186c379219','2023-07-17T02:19:59.107Z','2023-07-17T02:19:59.107Z');
 
 DROP TABLE IF EXISTS schedules;
-CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, date TEXT, text TEXT, createdAt TEXT, updatedAt TEXT);
+CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, date TEXT, text TEXT, user_id INTEGER, createdAt TEXT, updatedAt TEXT);
+INSERT INTO schedules (id, date, text, user_id, createdAt, updatedAt) VALUES (1, '2023-07-17T02:19:59.107Z', 'test', 2, '2023-07-17T02:19:59.107Z', '2023-07-17T02:19:59.107Z');
